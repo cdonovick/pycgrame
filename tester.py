df0d9840a90b92b4270607cabdde9e23785ab4b2
Caleb Donovick <donovick@cs.stanford.edu>
2018-10-17 13:53:34 -0700

    Tester script

--- tester.py
+++ tester.py
@@ -10,34 +10,42 @@ import constraints
 import optimization
 import modeler
 from util import Timer
+import json
 
-RESULTS_FILE = 'results.csv'
+RESULTS_FILE = 'results.json'
+
+SOLVER = 'Boolector'
+TIME_OUT = 2*60*60
 
 FABRICS = [
-    './fabrics/small_pattern.xml',
-    './fabrics/small_pattern_const.xml',
-    './fabrics/small_pattern_hetero.xml',
-    './fabrics/small_pattern_const_hetero.xml',
-#    './fabrics/mid_pattern.xml',
-#    './fabrics/mid_pattern_const.xml',
-#    './fabrics/mid_pattern_hetero.xml',
-#    './fabrics/mid_pattern_const_hetero.xml',
+    './benchmark_fabrics/small_ortho.xml',
+    './benchmark_fabrics/small_ortho_hetero.xml',
+    './benchmark_fabrics/small_diag.xml',
+    './benchmark_fabrics/small_diag_hetero.xml',
+    './benchmark_fabrics/mid_ortho.xml',
+    './benchmark_fabrics/mid_ortho_hetero.xml',
+    './benchmark_fabrics/mid_diag.xml',
+    './benchmark_fabrics/mid_diag_hetero.xml',
 ]
 
 DESIGNS = [
+    './designs/other/point_const.dot',
+    './designs/linalg/vv3x3.dot',
+    './designs/linalg/vcv3x3.dot',
+    './designs/linalg/vv4x4.dot',
+    './designs/linalg/vcv4x4.dot',
     './designs/linalg/vm2x2.dot',
     './designs/linalg/vcm2x2.dot',
     './designs/linalg/vm3x3.dot',
     './designs/linalg/vcm3x3.dot',
     './designs/linalg/mm2x2.dot',
     './designs/linalg/mcm2x2.dot',
-    './designs/linalg/vv3x3.dot',
-    './designs/linalg/vcv3x3.dot',
-    './designs/linalg/vv4x4.dot',
-    './designs/linalg/vcv4x4.dot',
 ]
 
-CONTEXTS = [1]
+CONTEXTS = [
+    1,
+    2,
+]
 
 OPTIMIZERS = {
 
@@ -52,24 +60,42 @@ OPTIMIZERS = {
                         optimization.limit_popcount_total),
 }
 
-INCREMENTAL = [True, False]
-
-SOLVERS = [
-    'Boolector',
+DUPLICATE = [
+    False,
+    True,
 ]
 
-CUTOFFS = [
-    0.0, 0.2, 2.0,
+CONFIG_MATS = [
+    {
+        'incremental' : [False],
+        'cutoff' : [None],
+        'optimize_final' : [False, True],
+    },
+    {
+        'incremental' : [True],
+        'cutoff' : [None],
+        'optimize_final' : [True],
+    },
+    {
+        'incremental' : [True],
+        'cutoff' : [0.0],
+        'optimize_final' : [False],
+    },
+    {
+        'incremental' : [True],
+        'cutoff' : [0.2, 0.5],
+        'optimize_final' : [False, True],
+    },
 ]
 
-fabric_cache = dict()
+
 solve_timer = Timer(time.perf_counter)
 build_timer = Timer(time.perf_counter)
 full_timer = Timer(time.perf_counter)
 init  = (
         constraints.init_placement_vars,
         constraints.init_routing_vars,
-    )
+)
 
 funcs = (
         constraints.op_placement,
@@ -81,17 +107,19 @@ funcs = (
         constraints.input_connectivity,
         constraints.output_connectivity,
         constraints.routing_resource_usage,
-    )
+)
 
-with open(RESULTS_FILE, 'a') as outfile:
-    date = datetime.datetime.now().isoformat(timespec='seconds')
-    outfile.write('\n\nrun_date, design, fabric, contexts, solver, incremental, cutoff, optimizer, total_time, total_build_time, total_solve_time, iterations, result, lower_bound, upper_bound;\n')
-    for design_file in DESIGNS:
-        for fabric_file in FABRICS:
-            for contexts in CONTEXTS:
-                for solver_str in SOLVERS:
-                    for incremental in INCREMENTAL:
-                        for cutoff in CUTOFFS:
+
+results = []
+
+date = datetime.datetime.now().isoformat(timespec='seconds')
+for fabric_file in FABRICS:
+    for contexts in CONTEXTS:
+        for design_file in DESIGNS:
+            for config_mat in CONFIG_MATS:
+                for incremental in config_mat['incremental']:
+                    for cutoff in config_mat['cutoff']:
+                        for optimize_final in config_mat['optimize_final']:
                             for optimizer_name, optimizer in OPTIMIZERS.items():
                                 solve_timer.reset()
                                 build_timer.reset()
@@ -99,8 +127,8 @@ with open(RESULTS_FILE, 'a') as outfile:
                                 mods, ties = dotparse.dot2graph(design_file)
                                 design = Design(mods, ties)
                                 cgra = adlparse(fabric_file)
-                                mrrg = fabric_cache[fabric_file, contexts] = MRRG(cgra, contexts=contexts)
-                                pnr = PNR(mrrg, design, solver_str, incremental=incremental)
+                                mrrg = MRRG(cgra, contexts=contexts)
+                                pnr = PNR(mrrg, design, SOLVER, incremental=incremental)
                                 full_timer.start()
                                 result = pnr.optimize_design(
                                         optimizer,
@@ -111,10 +139,34 @@ with open(RESULTS_FILE, 'a') as outfile:
                                         build_timer=build_timer,
                                         solve_timer=solve_timer,
                                         return_bounds=True,
+                                        optimize_final=optimize_final,
+                                        time_out=TIME_OUT,
                                         )
                                 full_timer.stop()
-                                if len(build_timer.times) != len(solve_timer.times):
-                                    print('something wierd happened')
-                                outfile.write(
-                                    f'{date}, {design_file}, {fabric_file}, {contexts}, {solver_str}, {incremental}, {cutoff}, {optimizer_name}, {full_timer.total}, {build_timer.total}, {solve_timer.total}, {len(solve_timer.times)}, {result[0]}, {result[1]}, {result[2]};\n')
-                                outfile.flush()
+                                results.append(
+                                    {
+                                        'params' : {
+                                            'fabric' : repr(fabric_file),
+                                            'contexts' : repr(contexts),
+                                            'design' : repr(design_file),
+                                            'incremental' : repr(incremental),
+                                            'cutoff' : repr(cutoff),
+                                            'optimize_final' : repr(optimize_final),
+                                            'optimizer' : repr(optimizer_name),
+                                        },
+                                        'results' : {
+                                            'sat' : repr(result[0]),
+                                            'lower' : repr(result[1]),
+                                            'upper' : repr(result[2]),
+                                            'total_time' : repr(full_timer.total),
+                                            'solve_time_total' : repr(solve_timer.total),
+                                            'build_time_total' : repr(build_timer.total),
+                                            'solve_times' : repr(tuple(solve_timer.times)),
+                                            'build_times' : repr(tuple(build_timer.times)),
+                                        },
+                                    }
+                                )
+
+with open(RESULTS_FILE, 'w') as f:
+    json.dump(results, f)
+
